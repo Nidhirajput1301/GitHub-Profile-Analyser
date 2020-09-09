d200fcc471e8cc00878234988d5433156a50fab3
Nidhirajput1301 <nidhirajput1301@gmail.com>
2020-09-09 14:47:03 +0530

    search user

--- src/context/context.js
+++ src/context/context.js
@@ -14,23 +14,55 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+
   // request loading
   const [requests, setRequests] = useState(0);
   const [loading, setIsLoading] = useState(false);
+
+  // error
+  const [error, setError] = useState({show: false, msg: ""});
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    // setLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).
+    catch(err => 
+      console.log(err)
+    );
+    //console.log(response);
+    if(response){
+      setGithubUser(response.data);
+      // more logic here
+    }
+    else{
+      toggleError(true, 'there is no user with that username')
+    }
+  };
+
   // check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
-    .then((data) => {
-      console.log(data);
+      .then(({ data }) => {
+      let {
+        rate:{ remaining }, 
+      } = data;
+      setRequests(remaining)
+      if(remaining === 0){
+        toggleError(true, "sorry, you have exceeded your hourly rate limit!");
+      }
     })
     .catch((err) => console.log(err));
   };
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+
   // error
   useEffect(checkRequests, []);
 
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests, error, searchGithubUser }}>
       {children}
     </GithubContext.Provider>
   );
